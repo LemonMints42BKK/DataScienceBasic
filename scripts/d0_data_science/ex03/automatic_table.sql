7df26f5e94c4f7ecfe652d261e205ac33105f65d
Pattaravadee N <65420071@kmitl.ac.th>
2025-04-27 11:26:32 +0700

    Day2 ex01 underconstuction

--- scripts/d0_data_science/ex03/automatic_table.sql
+++ scripts/d0_data_science/ex03/automatic_table.sql
@@ -1,35 +1,41 @@
--- CREATE OR REPLACE FUNCTION create_custom_table(tablename TEXT)
--- RETURNS void AS $$
--- BEGIN
---   EXECUTE format('
---     CREATE TABLE IF NOT EXISTS %I (
---         event_time TIMESTAMPTZ NOT NULL,
---         event_type TEXT,
---         product_id INTEGER,
---         price NUMERIC(10,2),
---         user_id BIGINT,
---         user_session UUID
---     )
---   ', tablename);
--- END;
--- $$ LANGUAGE plpgsql;
-
-
--- -- SELECT create_custom_table();
-CREATE FUNCTION get_record (query TEXT) 
-RETURNS RECORD AS $$
+DO $$
 DECLARE
-    rec RECORD;
+  file RECORD;
+  column_list TEXT;
+  first_row TEXT;
+  file_path TEXT;
+  table_name TEXT;
 BEGIN
-    EXECUTE query INTO rec;
-    RETURN rec; 
+  -- loop over CSV files in the /data directory
+  FOR file IN
+    SELECT filename
+    FROM (
+        SELECT pg_ls_dir('/data/subject/customer') AS filename
+    ) AS files
+    WHERE RIGHT(filename, 4) = '.csv'
+  LOOP
+    --prepare table name without .csv extention
+    table_name := regexp_replace(file.filename, '\.csv$', '', 'i');
+    --set file path
+    file_path := '/data/subject/customer/' || file.filename;
+    -- Create table if not exists
+    EXECUTE format('
+    CREATE TABLE IF NOT EXISTS %I (
+        event_time TIMESTAMPTZ NOT NULL,
+        event_type TEXT,
+        product_id INTEGER,
+        price NUMERIC(10,2),
+        user_id BIGINT,
+        user_session UUID
+    )', table_name);
+    -- Import data into the table (skip header)
+    EXECUTE format(
+        'COPY %I FROM %L WITH (FORMAT csv, HEADER true)', 
+        table_name, 
+        file_path
+        );
+
+        RAISE NOTICE 'Imported CSV: %', file.filename;
+  END LOOP;
 END;
 $$ LANGUAGE plpgsql;
-
-
-SELECT
-  *
-FROM
-  get_record (
-    'SELECT product_name, price FROM products WHERE product_id = 1'
-  ) AS product_info (product_name VARCHAR, price DEC);
--- scripts/d1_data_warehouse/ex01/customers_table.sh
+++ scripts/d1_data_warehouse/ex01/customers_table.sh
@@ -1,1 +1,45 @@
+CREATE OR REPLACE FUNCTION create_table(tablename TEXT)
+RETURNS void AS $$
+BEGIN
+  EXECUTE format('
+    CREATE TABLE IF NOT EXISTS %I (
+        event_time TIMESTAMPTZ NOT NULL,
+        event_type TEXT,
+        product_id INTEGER,
+        price NUMERIC(10,2),
+        user_id BIGINT,
+        user_session UUID
+    )
+  ', tablename);
+END;
+$$ LANGUAGE plpgsql;
+
+DO
+$$
+DECLARE
+    tbl1 RECORD;
+    tbl2 RECORD;
+    first_table_name TEXT;
+    customers_created BOOLEAN := false;
+BEGIN
+    -- Drop customers table if it already exists and create new one
+    EXECUTE 'DROP TABLE IF EXISTS customers';
+    PERFORM create_table('customers');
+    IF EXISTS (
+        SELECT 1
+        FROM pg_tables
+        WHERE schemaname = 'public'
+        AND tablename = 'customers') 
+    THEN
+        RAISE NOTICE 'Creating customers table';
+        customers_created := true;
+    END IF;
+
+
+
+    IF NOT customers_created THEN
+        RAISE NOTICE 'No matching tables found to merge!';
+    END IF;
+END
+$$ LANGUAGE plpgsql;
 
